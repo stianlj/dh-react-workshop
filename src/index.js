14f8e470db38de66bca4af84a9fd25333998753d
Stian Lund Johansen <stian.lund.johansen@nrk.no>
2016-09-26 15:13:29 +0200

    Remove files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+document.getElementById('root').innerHTML = '<h2>Hallo!</h2>';
